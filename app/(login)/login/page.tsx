e5cacec6c156e54c95738603c4b68521813272e8
DizzlyD <david4henery00@hotmail.com>
2025-08-03 21:00:37 +0100

    added-colour-theme

---
+++ app/(login)/login/page.tsx
@@ -0,0 +1,14 @@
+import LogoLarge from "@/public/assets/images/logo-large.svg";
+
+const page = () => {
+  return (
+    <main className="w-full min-h-svh">
+      <div className="w-full px-10 py-6 bg-black grid place-items-center rounded-b-lg">
+        <LogoLarge/>
+      </div>
+      
+    </main>
+  )
+}
+
+export default page
