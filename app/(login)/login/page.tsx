01f651687381b27aae66c630d72b20a4b5184605
DizzlyD <david4henery00@hotmail.com>
2025-08-05 20:53:59 +0100

    set-init-component-folders

--- app/(login)/login/page.tsx
+++ app/(login)/login/page.tsx
@@ -2,10 +2,13 @@ import LogoLarge from "@/public/assets/images/logo-large.svg";
 
 const page = () => {
   return (
-    <main className="w-full min-h-svh">
+    <main className="w-full min-h-svh bg-beige-100">
+
       <div className="w-full px-10 py-6 bg-black grid place-items-center rounded-b-lg">
         <LogoLarge/>
       </div>
+
+      
       
     </main>
   )
---
+++ components/login/LoginModal.tsx
@@ -0,0 +1,11 @@
+
+
+const LoginModal = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoginModal
