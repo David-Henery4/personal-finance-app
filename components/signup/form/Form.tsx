2d7c9cb3fcc616a480b9f78bcead8d2ace248237
DizzlyD <david4henery00@hotmail.com>
2025-08-29 21:43:01 +0100

    neon-client-setup

--- components/signup/form/Form.tsx
+++ components/signup/form/Form.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import { Input, SubmitBtn } from "@/components/reused/form-components"
+import { useStackApp } from "@stackframe/stack";
 
 const Form = () => {
+  const app = useStackApp();
+
   return (
     <form className="mt-8 flex flex-col gap-4">
       <Input id="name" label="name" name="name" />
