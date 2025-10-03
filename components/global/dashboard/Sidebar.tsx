ebdb3cd334871d8bc4bfa8e6078760e07e7df13e
DizzlyD <david4henery00@hotmail.com>
2025-10-03 21:11:01 +0100

    sidebar-styles

--- components/global/dashboard/Sidebar.tsx
+++ components/global/dashboard/Sidebar.tsx
@@ -2,14 +2,17 @@
 import { appRoutesList } from "@/local-data/appRoutes";
 import { usePathname } from "next/navigation";
 import SidebarLink from "./SidebarLink";
-
+import LogoLarge from "@/public/assets/images/logo-large.svg";
 
 const Sidebar = () => {
   const pathname = usePathname();
   return (
-    <menu className="w-full px-4 pt-2 rounded-t-lg sticky left-0 bottom-0 bg-grey-900 flex justify-between items-center sm-tablet:px-10 lg-laptop:flex-col lg-laptop:justify-start lg-laptop:rounded-tl-none">
-      {
-        appRoutesList.map((route) => {
+    <div className="w-full rounded-t-lg sticky left-0 bottom-0 bg-grey-900 lg-laptop:rounded-tl-none desktop-sm:min-w-[240px]">
+      <div className="hidden px-8 py-10 lg-laptop:block">
+        <LogoLarge />
+      </div>
+      <menu className="w-full px-4 pt-2 flex justify-between items-center sm-tablet:px-10 lg-laptop:flex-col lg-laptop:justify-start lg-laptop:p-0 lg-laptop:pr-6 lg-laptop:mt-6 lg-laptop:gap-1 lg-laptop:items-start">
+        {appRoutesList.map((route) => {
           return (
             <SidebarLink key={route.id} pathname={pathname} href={route.route}>
               <route.icon pathname={pathname} />
@@ -18,9 +21,9 @@ const Sidebar = () => {
               </p>
             </SidebarLink>
           );
-        })
-      }
-    </menu>
+        })}
+      </menu>
+    </div>
   );
 };
 
--- components/global/dashboard/SidebarLink.tsx
+++ components/global/dashboard/SidebarLink.tsx
@@ -16,13 +16,15 @@ const SidebarLink = ({
   return (
     <Link
       href={href}
-      className={`relative w-full grid place-items-center pt-2 pb-3 rounded-t-lg group hover:cursor-pointer ${
-        checkForActivePath() ? "bg-grey-100 text-grey-900" : "text-grey-300"
+      className={`relative capitalize w-full grid place-items-center pt-2 pb-3 rounded-t-lg group hover:scale-105 hover:cursor-pointer lg-laptop:px-8 lg-laptop:py-4 lg-laptop:rounded-tl-none lg-laptop:rounded-br-lg lg-laptop:grid-cols-dashboard-overview lg-laptop:justify-items-start lg-laptop:gap-4 ${
+        checkForActivePath()
+          ? "bg-grey-100 text-grey-900"
+          : "text-grey-300 hover:text-white"
       }`}
     >
       {children}
       {checkForActivePath() && (
-        <span className="absolute bottom-0 left-0 w-full h-1 bg-green"></span>
+        <span className="absolute bottom-0 left-0 w-full h-1 bg-green lg-laptop:top-0 lg-laptop:w-1 lg-laptop:h-full"></span>
       )}
     </Link>
   );
